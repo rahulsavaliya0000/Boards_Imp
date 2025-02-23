173f2ae6dc6c03b892cf7c3ad57e7f9fd34bb557
Rahul Savaliya <105914618+rahulsavaliya0000@users.noreply.github.com>
2025-02-24 03:17:21 +0530

    finallllllll

--- marry_me2.py
+++ marry_me2.py
@@ -206,7 +206,7 @@ def show_final_reveal():
 
 def main():
     inject_assets()
-    # st.title("Riza's Love Game 💘")
+    # st.title("MOST IMP Question's for Riza 💘")
 
     st.markdown("""
     <h1 class="title-animation" style="text-align: center; color: #C71585;">
